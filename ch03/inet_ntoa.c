752db9cb6c60027180746035bdc833fab8f0e95a
handongyexingren <1340551309@qq.com>
2022-04-09 12:55:37 +0800

    第三章，网络地址初始化

--- ch03/inet_ntoa.c
+++ ch03/inet_ntoa.c
@@ -11,10 +11,13 @@ int main(int argc, char *argv[])
     addr1.sin_addr.s_addr = htonl(0x1020304);
     addr2.sin_addr.s_addr = htonl(0x1010101);
     //把addr1中的结构体信息转换为字符串的IP地址形式
+
+    // ntoa返回的是字符串指针
     str_ptr = inet_ntoa(addr1.sin_addr);
     strcpy(str_arr, str_ptr);
     printf("Dotted-Decimal notation1: %s \n", str_ptr);
 
+    // 再次调用会修改原来字符串指针指向的字符串内容，因此str_ptr指向的字符串变了
     inet_ntoa(addr2.sin_addr);
     printf("Dotted-Decimal notation2: %s \n", str_ptr);
     printf("Dotted-Decimal notation3: %s \n", str_arr);
