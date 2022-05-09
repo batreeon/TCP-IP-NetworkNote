0763e277697eb43d2ee2d7f1af8efb23997f126a
handongyexingren <1340551309@qq.com>
2022-05-09 21:27:48 +0800

    第 8 章 域名及网络地址

--- ch08/gethostbyname.c
+++ ch08/gethostbyname.c
@@ -27,9 +27,10 @@ int main(int argc, char *argv[])
     printf("Address type: %s \n",
            (host->h_addrtype == AF_INET) ? "AF_INET" : "AF_INET6");
     // 输出ip地址信息
+    // host->h_addr_list[i]数组中的元素类型为 in_addr* 类型，但是结构体host中h_addr_list成员类型为char*
+    // 因此先强制类型转换，然后用*取内容
     for (i = 0; host->h_addr_list[i]; i++)
-        printf("IP addr %d: %s \n", i + 1,
-               inet_ntoa(*(struct in_addr *)host->h_addr_list[i]));
+        printf("IP addr %d: %s \n", i + 1, inet_ntoa(*(struct in_addr *)host->h_addr_list[i]));
     return 0;
 }
 void error_handling(char *message)
