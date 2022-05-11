e631f0db65d3acc207aafc77aa5529b9815e5697
batreeon <1340551309@qq.com>
2022-05-11 19:05:03 +0800

    第 14 章 多播与广播

--- ch14/news_sender_brd.c
+++ ch14/news_sender_brd.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
     broad_adr.sin_family = AF_INET;
     broad_adr.sin_addr.s_addr = inet_addr(argv[1]);
     broad_adr.sin_port = htons(atoi(argv[2]));
+    
     setsockopt(send_sock, SOL_SOCKET, SO_BROADCAST, (void *)&so_brd, sizeof(so_brd));
     if ((fp = fopen("news.txt", "r")) == NULL)
         error_handling("fopen() error");
